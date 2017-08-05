0f28ef725274080870a4c3e4dfd15be96ab828ce
Tomek <tomasz.maslon@gmail.com>
2017-08-05 12:40:33 +0200

    initial commit

---
+++ app/src/main/java/com/pum/tomasz/gpx_tracks/MainActivity.java
@@ -0,0 +1,13 @@
+package com.pum.tomasz.gpx_tracks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
